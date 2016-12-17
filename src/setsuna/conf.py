5079b6acf87885e25625d6b934dfc021afa2a77f
nukorou <nukorou3@gmail.com>
2016-12-17 12:00:34 +0900

    mongodb settings

--- src/setsuna/conf.py
+++ src/setsuna/conf.py
@@ -1,11 +1,13 @@
 from pymongo import MongoClient
 import configparser
 import os
+from urllib import parse
 
 if os.environ.get('PRODUCTION'):
     # 本番設定
     # port_number = os.environ.get('DB_PORT')
-    connect = MongoClient(os.environ.get('REDISTOGO_URL'))
+    MONGO_URL = os.environ.get('MONGODB_URI')
+    connect = MongoClient(MONGO_URL)
     posts = connect['setsuna']['posts']
     life = 6
 else:
