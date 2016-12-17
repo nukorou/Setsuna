aa375b53c3f6e2d4a307586588a30636a91dc3ac
nukorou <nukorou3@gmail.com>
2016-12-17 11:37:23 +0900

    heroku settings

--- src/setsuna/conf.py
+++ src/setsuna/conf.py
@@ -1,15 +1,23 @@
 from pymongo import MongoClient
 import configparser
+import os
 
-# Import config
-config = configparser.ConfigParser()
-config.read('setsuna.cfg')
-conf = config['laptop']
-port_number = int(conf['port'])
+if os.environ.get('PRODUCTION'):
+    # 本番設定
+    # port_number = os.environ.get('DB_PORT')
+    connect = MongoClient(os.environ.get('REDISTOGO_URL'))
+    posts = connect['setsuna']['posts']
+    life = 6
+else:
+    # Import config
+    config = configparser.ConfigParser()
+    config.read('setsuna.cfg')
+    conf = config['laptop']
+    port_number = int(conf['port'])
 
-# DB Connection
-connect = MongoClient(conf['address'], port_number)
-posts = connect[conf['database']][conf['collection']]
+    # DB Connection
+    connect = MongoClient(conf['address'], port_number)
+    posts = connect[conf['database']][conf['collection']]
 
-# Setsuna configp
-life = int(conf['life'])
+    # Setsuna configp
+    life = int(conf['life'])
