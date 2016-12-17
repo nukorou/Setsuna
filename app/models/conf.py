e0630564cb1ee736aacfbe9a4bd1071b1ec75509
nukorou <nukorou3@gmail.com>
2016-12-17 15:57:26 +0900

    mongodb 接続設定

--- app/models/conf.py
+++ app/models/conf.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import os
+from urllib.parse import urlparse
 
 # 生存時間
 life = 6
@@ -7,8 +8,9 @@ life = 6
 if os.environ.get('PRODUCTION'):
     # 本番設定
     MONGO_URL = os.environ.get('MONGODB_URI')
+    db_name = urlparse(MONGO_URL).path.replace('/', '')
     connect = MongoClient(MONGO_URL)
-    posts = connect['setsuna']['posts']
+    posts = connect[db_name]['posts']
 else:
     # ローカル設定
     connect = MongoClient('127.0.0.1', 27017)
