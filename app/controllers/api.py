49766cf06b2687d87ed6320a4f8a063384d84f37
nukorou <nukorou3@gmail.com>
2016-12-18 11:18:20 +0900

    jsonに時間の変換をサポートするように変更 メソッドのリネーム importの最適化 エラーの出力 pep8準拠

--- app/controllers/api.py
+++ app/controllers/api.py
@@ -1,7 +1,8 @@
 import json
 from datetime import datetime
-from ..models import post_factory, post, response_post, posts
+from ..models import post, response_post, posts
 from flask import abort, Request, request, Response, Blueprint
+from ..models.post_factory import post_factory
 
 app = Blueprint('api', __name__)
 
@@ -11,6 +12,7 @@ _DATE_FORMAT = '%Y%m%dT%H%M%S%z'
 
 @app.errorhandler(400)
 def bad_request(error):
+    print(error)
     err_obj = _err_dict['errors'].append({'status': '400',
                                           'title': 'Bad Request',
                                           'detail': 'Sorry, I can not get request.'})
@@ -27,6 +29,7 @@ def password_not_match():
 
 @app.errorhandler(404)
 def not_found(error):
+    print(error)
     err_obj = _err_dict['errors'].append({'status': '400',
                                           'title': 'Not Found',
                                           'detail': 'Your request page is not found.'})
@@ -35,6 +38,7 @@ def not_found(error):
 
 @app.errorhandler(410)
 def not_found(error):
+    print(error)
     err_obj = _err_dict['errors'].append({'status': '410',
                                           'title': 'Gone',
                                           'detail': 'This post was deleted maybe.'})
@@ -43,18 +47,25 @@ def not_found(error):
 
 @app.errorhandler(500)
 def internal_error(error):
+    print(error)
     err_obj = _err_dict['errors'].append({'status': '500',
                                           'title': 'Internal Server Error',
                                           'detail': 'Sorry, Internal server error...'})
     return Response(json.dumps(err_obj), 500)
 
 
-def know_post(uid):
-    is_post = post_factory.post_factory(uid)
+def exist_post(uid):
+    is_post = post_factory(uid)
     if is_post is not None:
         return True
     else:
         return False
+
+
+def support_datetime_default(o):
+    if isinstance(o, datetime):
+        return o.isoformat()
+    raise TypeError(repr(o) + " is not JSON serializable")
 
 
 # APIの説明書
@@ -71,7 +82,7 @@ def get_all_posts():
     tmp_posts = posts.Posts()
     tmp_posts.get_all_posts()
 
-    return Response(json.dumps(tmp_posts), 200)
+    return Response(json.dumps(tmp_posts, default=support_datetime_default), 200)
 
 
 # リミットの数まで取得する
@@ -80,7 +91,7 @@ def get_posts_by_limit(limit: int):
     tmp_posts = posts.Posts()
     tmp_posts.get_posts_by_limit(limit)
 
-    return Response(json.dumps(tmp_posts), 200)
+    return Response(json.dumps(tmp_posts, default=support_datetime_default), 200)
 
 
 # 日付を指定して刹那を得る
@@ -90,7 +101,7 @@ def get_posts_between(datetime_s: str, datetime_e: str):
     tmp_posts.get_posts_between(datetime.strptime(datetime_s, _DATE_FORMAT),
                                 datetime.strptime(datetime_e, _DATE_FORMAT))
 
-    return Response(json.dumps(tmp_posts), 200)
+    return Response(json.dumps(tmp_posts, default=support_datetime_default), 200)
 
 
 # 指定した言語の刹那を取得する
@@ -99,7 +110,7 @@ def get_posts_by_lang(lang: str):
     tmp_posts = posts.Posts()
     tmp_posts.get_posts_by_lang(lang)
 
-    return Response(json.dumps(tmp_posts), 200)
+    return Response(json.dumps(tmp_posts, default=support_datetime_default), 200)
 
 
 # 数と言語を指定して刹那を取得する
@@ -108,7 +119,7 @@ def get_posts_by_lang_and_limit(lang: str, limit: int):
     tmp_posts = posts.Posts()
     tmp_posts.get_posts_by_lang_and_limit(lang, limit)
 
-    return Response(json.dumps(tmp_posts), 200)
+    return Response(json.dumps(tmp_posts, default=support_datetime_default), 200)
 
 
 # 言語と日付を指定して刹那を取得する
@@ -117,15 +128,15 @@ def get_posts_by_lang_and_between(datetime_s: str, datetime_e: str, lang: str):
     tmp_posts = posts.Posts()
     tmp_posts.get_posts_by_lang_and_between(datetime_s, datetime_e, lang)
 
-    return Response(json.dumps(vars(tmp_posts)), 200)
+    return Response(json.dumps(vars(tmp_posts), default=support_datetime_default), 200)
 
 
 # uid指定の刹那を１つ
 @app.route('/api/post/<uid>', methods=['GET'])
 def get_post(uid: str):
-    tmp_post = post_factory.post_factory(uid)
+    tmp_post = post_factory(uid)
 
-    return Response(json.dumps(vars(tmp_post)), 200)
+    return Response(json.dumps(vars(tmp_post), default=support_datetime_default), 200)
 
 
 # 投稿
@@ -136,7 +147,7 @@ def post_content():
     if not req:
         abort(400)
 
-    if not 'content' in req:
+    if 'content' not in req:
         abort(400)
 
     if len(req['content']) <= 0:
@@ -147,17 +158,17 @@ def post_content():
                          lang=req['lang'] if 'lang' in req else 'und')
     tmp_post.post_contribution()
 
-    return Response(json.dumps(vars(tmp_post)), 200)
+    return Response(json.dumps(vars(tmp_post), default=support_datetime_default), 200)
 
 
-# 延命
+# 返信->延命,ふぁぼｰ>延命
 @app.route('/api/post/<uid>', methods=['POST'])
 def res_post(uid: str):
-    if know_post(uid):
+    if exist_post(uid):
         if not Request.get_json(request):
-            apo_post = post_factory.post_factory(uid)
+            apo_post = post_factory(uid)
             apo_post.apothanasia()
-            return Response(json.dumps(vars(apo_post)), 200)
+            return Response(json.dumps(vars(apo_post), default=support_datetime_default), 200)
 
         req = Request.get_json(request)
         tmp_post = response_post.ResponsePost(link=uid, content=req['content'],
@@ -166,20 +177,20 @@ def res_post(uid: str):
                                               lang=req['lang'] if 'lang' in Request.get_json(request) else 'und')
         tmp_post.post_contribution()
 
-        return Response(json.dumps(vars(tmp_post)), 200)
+        return Response(json.dumps(vars(tmp_post), default=support_datetime_default), 200)
 
 
 # 削除
 @app.route('/api/post/<uid>', methods=['DELETE'])
 def delete_post(uid):
-    if know_post(uid):
+    if exist_post(uid):
         if not Request.get_json(request):
-            req = Request.get_Json(request)
-            if not 'password' in req:
+            req = Request.get_json(request)
+            if 'password' not in req:
                 abort(400)
 
         req = Request.get_json(request)
-        tmp_post = post_factory.post_factory(uid)
+        tmp_post = post_factory(uid)
         if not tmp_post.password == req['password']:
             abort(401)
         else:
