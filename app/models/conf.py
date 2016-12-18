60363683c72d4c72747d2d6a7613d29dffd47455
nukorou <nukorou3@gmail.com>
2016-12-18 11:19:36 +0900

    ローカルの生存時間とプロダクションの生存時間の定義を別々に 時間を秒に変更

--- app/models/conf.py
+++ app/models/conf.py
@@ -2,8 +2,7 @@ from pymongo import MongoClient
 import os
 from urllib.parse import urlparse
 
-# 生存時間
-life = 6
+
 
 if os.environ.get('PRODUCTION'):
     # 本番設定
@@ -11,7 +10,17 @@ if os.environ.get('PRODUCTION'):
     db_name = urlparse(MONGO_URL).path.replace('/', '')
     connect = MongoClient(MONGO_URL)
     posts = connect[db_name]['posts']
+
+    # 生存時間
+    life = 6 * 3600
+    # 伸ばす時間
+    extend_time = 1 * 3600
 else:
     # ローカル設定
     connect = MongoClient('127.0.0.1', 27017)
     posts = connect['setsuna']['posts']
+
+    # 生存時間
+    life = 1
+    # 伸ばす時間
+    extend_time = 1
