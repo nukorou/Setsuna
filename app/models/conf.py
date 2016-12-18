f6e2afc8c761cf6f65e82fcc6164178a8bfd6808
nukorou <nukorou3@gmail.com>
2016-12-18 11:45:33 +0900

    index貼るのを移動 bundle.js

--- app/models/conf.py
+++ app/models/conf.py
@@ -2,24 +2,24 @@ from pymongo import MongoClient
 import os
 from urllib.parse import urlparse
 
-
-
 if os.environ.get('PRODUCTION'):
     # 本番設定
     MONGO_URL = os.environ.get('MONGODB_URI')
     db_name = urlparse(MONGO_URL).path.replace('/', '')
     connect = MongoClient(MONGO_URL)
     posts = connect[db_name]['posts']
-
+    posts.create_index("deleted_at", expireAfterSeconds=60)
     # 生存時間
     life = 6 * 3600
     # 伸ばす時間
     extend_time = 1 * 3600
+
+
 else:
     # ローカル設定
     connect = MongoClient('127.0.0.1', 27017)
     posts = connect['setsuna']['posts']
-
+    posts.create_index("deleted_at", expireAfterSeconds=60)
     # 生存時間
     life = 1
     # 伸ばす時間
